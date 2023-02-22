d1fc4fdab15469a2c7fe4b7ebe75836e2364e0f8
IniFadhil <125639169+IniFadhil@users.noreply.github.com>
2023-02-22 19:23:49 +0700

    Add files via upload

---
+++ kuliah/pertemuan2/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>
+    <?php echo 'Hello World'; ?>
+    </h1>
+    <h3>
+        <?php echo 'pemrograman web'; ?>
+    </h3>
+    <hr>
+<?php 
+ $nama_depan="Fadhil";
+ $nama_belakang="Rizki Fauzan";
+?>
+    <h2>
+        <?php echo "Hello, $nama_depan $nama_belakang!"; ?>
+    </h2>
+</body>
+</html>
